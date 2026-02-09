1379baf2412a45ae011c524acc0ca2e741510ae2
abishekpandey958-source <abishekpandey958@gmail.com>
2026-02-10 04:33:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,45 @@
-const CACHE_NAME = "atd-calculator-v1";
-
+const CACHE_NAME = "atd-calc-v3"; // change number when updating
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
   "./manifest.json",
-  "./app-icon.png"
+  "./app-icon.png",
+  "./splash-icon.png"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", (event) => {
   self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
