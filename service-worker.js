d6e981c1a449f2a8c4caa49ea1888583cf00e51c
abishekpandey958-source <abishekpandey958@gmail.com>
2026-02-10 15:22:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,20 @@
-const cacheName = 'atd-calc-v6'; // Incremented version
+const cacheName = 'atd-final-v1';
 const assets = [
   './',
   './index.html',
-  './style.css',        /* Added your external CSS to the cache */
-  './manifest.json',
-  './app-icon.png',     /* Added icons so the home screen looks right offline */
-  './splash-icon.png',
-  'https://cdn.tailwindcss.com' 
+  './manifest.json'
 ];
 
-// Install Event: Saving files to the browser cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
-// Activate Event: Deleting old cache versions
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== cacheName) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.map((k) => k !== cacheName ? caches.delete(k) : null));
+  }));
 });
 
-// Fetch Event: Serving cached content when offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      // Return cached file if found, otherwise fetch from network
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
