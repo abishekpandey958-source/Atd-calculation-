ff7363031b818c87827c89ef1c163bddde38e3d7
abishekpandey958-source <abishekpandey958@gmail.com>
2026-02-10 21:24:50 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Incrementing to v3 to force the browser to download the new precision fix
-const cacheName = 'atd-final-v6'; 
+const cacheName = 'atd-final-v7'; 
 const assets = [
   './',
   './index.html',
