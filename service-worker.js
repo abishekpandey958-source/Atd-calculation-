08035f72297ba654a0c2531b045f4433794034a8
abishekpandey958-source <abishekpandey958@gmail.com>
2026-02-10 06:53:21 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,40 @@
-const cacheName = 'atd-calc-v1';
-const assets = ['./', './index.html'];
+const cacheName = 'atd-calc-v2'; // Incremented version
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com' // Caching the external Tailwind script for offline use
+];
 
+// Install: Saving files to the phone's memory
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Activate: Cleaning up old cache versions
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: Serving files from memory when offline
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
