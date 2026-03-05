3250e2dad04525c516fc7baa3567855a3ae231c3
abishekpandey958-source <abishekpandey958@gmail.com>
2026-03-05 14:45:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Incrementing to v3 to force the browser to download the new precision fix
-const cacheName = 'atd-final-v8'; 
+const cacheName = 'atd-calculator-v1.5'; 
 const assets = [
   './',
   './index.html',
